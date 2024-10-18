652bb84543c42ff7d0225534b4449660409c1982
chrys <chrystopher_allan@yahoo.com.br>
2024-10-18 15:02:10 -0300

    Creating player shot Creating enemy shot

--- code/Const.py
+++ code/Const.py
@@ -16,10 +16,15 @@ ENTITY_SPEED = {
     'Level1Bg5': 5,
     'Level1Bg6': 6,
     'Player1': 3,
+    'Player1Shot': 2,
     'Player2': 3,
-    'Enemy1': 2,
+    'Player2Shot': 2,
+    'Enemy1': 1,
+    'Enemy1Shot': 5,
     'Enemy2': 1,
-    'Enemy3': 4,
+    'Enemy2Shot': 2,
+    'Enemy3': 2,
+    'Enemy3Shot': 4,
 }
 
 ENTITY_HEALTH = {
@@ -31,9 +36,22 @@ ENTITY_HEALTH = {
     'Level1Bg5': 999,
     'Level1Bg6': 999,
     'Player1': 300,
+    'Player1Shot': 1,
     'Player2': 300,
+    'Player2Shot': 1,
     'Enemy1': 50,
+    'Enemy1Shot': 1,
     'Enemy2': 60,
+    'Enemy2Shot': 2,
+    'Enemy3': 80,
+    'Enemy3Shot': 3,
+}
+
+ENTITY_SHOT_DELAY = {
+    'Player1': 20,
+    'Player2': 15,
+    'Enemy1': 100,
+    'Enemy2': 200,
     'Enemy3': 100,
 }
 
--- code/EntityMediator.py
+++ code/EntityMediator.py
@@ -1,11 +1,21 @@
+from code.EnemyShot import EnemyShot
+from code.Const import WIN_WIDTH
 from code.Enemy import Enemy
 from code.Entity import Entity
+from code.PlayerShot import PlayerShot
+
 
 class EntityMediator:
 
     @staticmethod
     def __verify_collision_window(ent: Entity):
         if isinstance(ent, Enemy):
+            if ent.rect.right < 0:
+                ent.health = 0
+        if isinstance(ent, PlayerShot):
+            if ent.rect.left >= WIN_WIDTH:
+                ent.health = 0
+        if isinstance(ent, EnemyShot):
             if ent.rect.right < 0:
                 ent.health = 0
 
@@ -16,7 +26,7 @@ class EntityMediator:
             EntityMediator.__verify_collision_window(test_entity)
 
     @staticmethod
-    def verify_collision(entity_list: list[Entity]):
+    def verify_health(entity_list: list[Entity]):
         for ent in entity_list:
             if ent.health <=0:
                 entity_list.remove(ent)
